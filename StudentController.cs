9625d0cc0aab51be6477ed0c34faa4112afff150
ajaygkumar <31571077+ajaygkumar@users.noreply.github.com>
2017-09-27 00:50:13 -0500

    Add files via upload

---
+++ StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvc.Models;
+
+namespace mvc.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Student()
+        {
+            Student student = new Student();
+            student.id = 1;
+            student.name = "Ajay Kumar";
+            student.age = 24;
+            student.course = "CS";
+            return View(student);
+        }
+    }
+}
